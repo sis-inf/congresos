aa6e4e237b95c1a9e2fac423f8ed14171d9c89a1
marcel06A <arratia.barco06@gmail.com>
2025-06-19 19:02:23 -0400

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('',inicio),
+    
 ]
